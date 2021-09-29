83b9e4359134e1b486d79749c70eb5bea7e47c2e
John Fa <john6fa@gmail.com>
2021-09-28 23:38:23 -0700

    schema fix

--- schema.sql
+++ schema.sql
@@ -1,86 +1,105 @@
--- ---
--- Globals
--- ---
-
--- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
--- SET FOREIGN_KEY_CHECKS=0;
-
--- ---
--- Table 'products'
---
--- ---
-
 CREATE DATABASE qanda;
 
-DROP TABLE IF EXISTS `products`;
+DROP TABLE IF EXISTS products;
 
-CREATE TABLE `products` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE products (
+  id sSERIAL PRIMARY KEY
 );
 
--- ---
--- Table 'questions'
---
--- ---
+DROP TABLE IF EXISTS questions;
 
-DROP TABLE IF EXISTS `questions`;
-
-CREATE TABLE `questions` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `question_body` VARCHAR(1000) NULL DEFAULT NULL,
-  `question_date` DATE NULL DEFAULT NULL,
-  `asker_name` VARCHAR(60) NULL DEFAULT NULL,
-  `asker_email` VARCHAR(60) NULL DEFAULT NULL,
-  `question_helpfulness` INTEGER NULL DEFAULT NULL,
-  `question_reported` VARCHAR(Boolean) NULL DEFAULT 'false',
-  `product_id` INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE questions (
+  id SERIAL PRIMARY KEY,
+  question_body VARCHAR(1000) NOT NULL,
+  question_date DATE default current_timestamp,
+  asker_name VARCHAR(60) NOT NULL,
+  asker_email VARCHAR(60) NOT NULL,
+  question_helpfulness INT NOT NULL default 0,
+  reported boolean default false,
+  product_id INT references products(id)
 );
 
--- ---
--- Table 'answers'
---
--- ---
+DROP TABLE IF EXISTS answers;
 
-DROP TABLE IF EXISTS `answers`;
-
-CREATE TABLE `answers` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `question_id` INTEGER NULL DEFAULT NULL,
-  `answerer_name` VARCHAR(60) NULL DEFAULT NULL,
-  `answerer_email` VARCHAR(60) NULL DEFAULT NULL,
-  `answer_body` VARCHAR(1000) NULL DEFAULT NULL,
-  `answer_date` DATE NULL DEFAULT NULL,
-  `answer_helpfulness` INTEGER NULL DEFAULT NULL,
-  `answer_reported` VARCHAR(Boolean) NULL DEFAULT 'false',
-  PRIMARY KEY (`id`)
+CREATE TABLE answers (
+  id SERIAL PRIMARY KEY,
+  answer_body VARCHAR(1000) NOT NULL,
+  created_at DATE default current_timestamp,
+  answerer_name VARCHAR(60) NOT NULL,
+  answerer_email VARCHAR(60) NOT NULL,
+  helpfulness INT default 0,
+  reported boolean default false,
+  question_id INT references questions(id)
 );
 
--- ---
--- Table 'photos'
---
--- ---
+DROP TABLE IF EXISTS photos;
 
-DROP TABLE IF EXISTS `photos`;
-
-CREATE TABLE `photos` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `answer_id` INTEGER NULL DEFAULT NULL,
-  `url` VARCHAR(250) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE photos (
+  id SERIAL PRIMARY KEY,
+  url VARCHAR(250) NOT NULL,
+  answer_id INT references answers(id)
 );
+
+-- COPY answers FROM '/Users/JohnFa 1/Desktop/Hack Reactor/SDC/questionsAPI/data/answers.csv' DELIMITER ',' CSV HEADER;
+-- COPY answers_photos FROM '/Users/JohnFa 1/Desktop/Hack Reactor/SDC/questionsAPI/data/answers_photos.csv' DELIMITER ',' CSV HEADER;
+-- COPY questions FROM '/Users/JohnFa 1/Desktop/Hack Reactor/SDC/questionsAPI/data/questions.csv' DELIMITER ',' CSV HEADER;
+
+
+
+-- CREATE TABLE `products` (
+--   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+--   PRIMARY KEY (`id`)
+-- );
+
+-- DROP TABLE IF EXISTS `questions`;
+
+-- CREATE TABLE `questions` (
+--   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+--   `question_body` VARCHAR(1000) NULL DEFAULT NULL,
+--   `question_date` DATE NULL DEFAULT NULL,
+--   `asker_name` VARCHAR(60) NULL DEFAULT NULL,
+--   `asker_email` VARCHAR(60) NULL DEFAULT NULL,
+--   `question_helpfulness` INTEGER NULL DEFAULT NULL,
+--   `question_reported` VARCHAR(Boolean) NULL DEFAULT 'false',
+--   `product_id` INTEGER NULL DEFAULT NULL,
+--   PRIMARY KEY (`id`)
+-- );
+
+
+-- DROP TABLE IF EXISTS `answers`;
+
+-- CREATE TABLE `answers` (
+--   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+--   `question_id` INTEGER NULL DEFAULT NULL,
+--   `answerer_name` VARCHAR(60) NULL DEFAULT NULL,
+--   `answerer_email` VARCHAR(60) NULL DEFAULT NULL,
+--   `answer_body` VARCHAR(1000) NULL DEFAULT NULL,
+--   `answer_date` DATE NULL DEFAULT NULL,
+--   `answer_helpfulness` INTEGER NULL DEFAULT NULL,
+--   `answer_reported` VARCHAR(Boolean) NULL DEFAULT 'false',
+--   PRIMARY KEY (`id`)
+-- );
+
+
+-- DROP TABLE IF EXISTS `photos`;
+
+-- CREATE TABLE `photos` (
+--   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+--   `answer_id` INTEGER NULL DEFAULT NULL,
+--   `url` VARCHAR(250) NULL DEFAULT NULL,
+--   PRIMARY KEY (`id`)
+-- );
 
 -- ---
 -- Foreign Keys
 -- ---
 
-ALTER TABLE `questions` ADD FOREIGN KEY (product_id) REFERENCES `products` (`id`);
-ALTER TABLE `answers` ADD FOREIGN KEY (question_id) REFERENCES `questions` (`id`);
-ALTER TABLE `photos` ADD FOREIGN KEY (answer_id) REFERENCES `answers` (`id`);
+-- ALTER TABLE `questions` ADD FOREIGN KEY (product_id) REFERENCES `products` (`id`);
+-- ALTER TABLE `answers` ADD FOREIGN KEY (question_id) REFERENCES `questions` (`id`);
+-- ALTER TABLE `photos` ADD FOREIGN KEY (answer_id) REFERENCES `answers` (`id`);
 
 
-COPY answers FROM '/Users/JohnFa 1/Desktop/Hack Reactor/SDC/questionsAPI/data/answers.csv' DELIMITER ',' CSV HEADER;
+
 
 
 
